2b2f339d174954e4e305d495e2d4ca3c4380db32
Anon Dev <michaelerastus9@gmail.com>
2024-07-19 08:23:31 +0100

    Added livechat

--- resources/views/company/layout/base.blade.php
+++ resources/views/company/layout/base.blade.php
@@ -200,12 +200,15 @@
 <script src="{{asset('home/js/custom.min.js')}}" defer></script>
 <script src="//cdn.jsdelivr.net/npm/sweetalert2@11" defer></script>
 <x-livewire-alert::scripts />
-<!-- Start of LiveAgent integration script: Invitations -->
+<!-- Begin of Chaport Live Chat code -->
 <script type="text/javascript">
-    (function(d, src, c) { var t=d.scripts[d.scripts.length - 1],s=d.createElement('script');s.id='la_x2s6df8d';s.defer=true;s.src=src;s.onload=s.onreadystatechange=function(){var rs=this.readyState;if(rs&&(rs!='complete')&&(rs!='loaded')){return;}c(this);};t.parentElement.insertBefore(s,t.nextSibling);})(document,
-        'https://xulfashion.ladesk.com/scripts/track.js',
-        function(e){  });
+    (function(w,d,v3){
+        w.chaportConfig = {
+            appId : '669a12b3f04cd26a66596d52'
+        };
+
+        if(w.chaport)return;v3=w.chaport={};v3._q=[];v3._l={};v3.q=function(){v3._q.push(arguments)};v3.on=function(e,fn){if(!v3._l[e])v3._l[e]=[];v3._l[e].push(fn)};var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://app.chaport.com/javascripts/insert.js';var ss=d.getElementsByTagName('script')[0];ss.parentNode.insertBefore(s,ss)})(window, document);
 </script>
-<!-- End of LiveAgent integration script -->
+<!-- End of Chaport Live Chat code -->
 </body>
 </html>
