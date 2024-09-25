6a1221721551e04c92f9debbebc47431982698fb
Anon Dev <michaelerastus9@gmail.com>
2024-09-25 11:16:49 +0100

    Added preloader

--- resources/views/mobile/ads/layout/base.blade.php
+++ resources/views/mobile/ads/layout/base.blade.php
@@ -100,9 +100,9 @@
 </head>
 
 <body>
-{{--<div class="loader-wrapper">--}}
-{{--    <span class="loader"></span>--}}
-{{--</div>--}}
+<div class="loader-wrapper">
+    <span class="loader"></span>
+</div>
 
 @include('mobile.ads.layout.topSection')
 
