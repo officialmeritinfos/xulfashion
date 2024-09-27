ddf0880c752a3f8e4328a1cb0a1437188fbf79ae
Anon Dev <michaelerastus9@gmail.com>
2024-09-27 08:52:09 +0100

    Updated preloader color

--- resources/views/genericCss.blade.php
+++ resources/views/genericCss.blade.php
@@ -13,7 +13,7 @@
         left: 0;
         width: 100%;
         height: 100%;
-        background: transparent;
+        background: inherit;
         z-index: 9999;
         display: flex;
         justify-content: center;
