f7ae2ddca6192e9f35164a7735b533be39192cb4
Anon Dev <michaelerastus9@gmail.com>
2024-11-10 20:05:45 +0100

    Updated Keys and moved them to ENV file

--- config/constant.php
+++ config/constant.php
@@ -13,21 +13,20 @@ return [
         'live'      =>true
     ],
     'flutterwave'=>[
-        'testPubKey'    =>'FLWPUBK_TEST-947de2621588b85de1ad6eb68ef4a1b3-X',
-        'testSecKey'    =>'FLWSECK_TEST-69264747535477addbd3996cfa57c3c7-X',
-        'testEncKey'    =>'FLWSECK_TEST17aea0267b88',
+        'testPubKey'    =>env('FLUTTERWAVE_TEST_PUBLIC_KEY'),
+        'testSecKey'    =>env('FLUTTERWAVE_TEST_SECRET_KEY'),
         'url'           =>'https://api.flutterwave.com/v3/',
         'secHash'       =>'*****',
-        'pubKey'        =>'*****',
-        'secKey'        =>'*****',
+        'pubKey'        =>env('FLUTTERWAVE_PUBLIC_KEY'),
+        'secKey'        =>env('FLUTTERWAVE_SECRET_KEY'),
         'encKey'        =>'******',
         'live'          =>false
     ],
     'paystack'=>[
-        'testSecKey'    =>'sk_test_75f6ba26c72eebd5e3ba53fbec828a85969b6f52',
-        'testPubKey'    =>'pk_test_2f3096160be1e626bd011224914b5ada0c7107ff',
-        'liveSecKey'    =>'sk_live_3bca78c0aeddda528e4c1dfcd8e9d6129eb99b4d',
-        'livePubKey'    =>'pk_live_7091a927346051cb11fe8cff64f32f783e2a3bdd',
+        'testSecKey'    =>env('PAYSTACK_TEST_SECRET_KEY'),
+        'testPubKey'    =>env('PAYSTACK_TEST_SECRET_KEY'),
+        'liveSecKey'    =>env('PAYSTACK_SECRET_KEY'),
+        'livePubKey'    =>env('PAYSTACK_PUBLIC_KEY'),
         'live'          =>false,
         'url'           =>'https://api.paystack.co/'
     ]
