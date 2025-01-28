016d3d9aa3245bbc804e1ef93730aec2e0b6f881
Anon Dev <michaelerastus9@gmail.com>
2025-01-28 14:38:34 +0100

    Completed work on MVP

--- resources/views/company/layout/base.blade.php
+++ resources/views/company/layout/base.blade.php
@@ -334,17 +334,17 @@
     @stack('js')
     <x-livewire-alert::scripts />
     <!--Start of Tawk.to Script-->
-{{--    <script type="text/javascript">--}}
-{{--        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();--}}
-{{--        (function(){--}}
-{{--            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];--}}
-{{--            s1.async=true;--}}
-{{--            s1.src='https://embed.tawk.to/669a2d06becc2fed69278138/1i353vojt';--}}
-{{--            s1.charset='UTF-8';--}}
-{{--            s1.setAttribute('crossorigin','*');--}}
-{{--            s0.parentNode.insertBefore(s1,s0);--}}
-{{--        })();--}}
-{{--    </script>--}}
+    <script type="text/javascript">
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/669a2d06becc2fed69278138/1i353vojt';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+        })();
+    </script>
     <script>
         $(function (){
             window.Tawk_API.onLoad = function() {
--- resources/views/company/layouts/base.blade.php
+++ resources/views/company/layouts/base.blade.php
@@ -181,30 +181,6 @@
 <script src="{{asset('home/main/vendors/parallax/jquery.parallax-scroll.js')}}"></script>
 <script src="{{asset('home/main/vendors/wow/wow.min.js')}}"></script>
 <script src="{{asset('home/main/js/custom.js')}}"></script>
-
-<!--Start of Tawk.to Script-->
-<script type="text/javascript">
-    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-    (function(){
-        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-        s1.async=true;
-        s1.src='https://embed.tawk.to/669a2d06becc2fed69278138/1i353vojt';
-        s1.charset='UTF-8';
-        s1.setAttribute('crossorigin','*');
-        s0.parentNode.insertBefore(s1,s0);
-    })();
-</script>
-<script>
-    $(function (){
-        window.Tawk_API.onLoad = function() {
-            window.Tawk_API.hideWidget();
-        }
-    });
-    $('.startChat').on('click',function (){
-        window.Tawk_API.popup();
-    })
-</script>
-<!--End of Tawk.to Script-->
 @include('basicInclude')
 </body>
 </html>
